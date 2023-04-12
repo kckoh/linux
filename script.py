c5a5e654c15acff6400d4979864e36e295959e7b
kckoh <kyungcheolkoh@gmail.com>
2023-04-12 03:14:47 +0000

    finish the work

--- script.py
+++ script.py
@@ -1,1 +1,19 @@
-print("hello world")
+import glob
+import pandas as pd
+
+# Get a list of all CSV files in the current directory
+csv_files = glob.glob("./docs/*.csv")
+
+# Create an empty list to store the dataframes
+df_list = []
+
+# Loop through the CSV files and read each one into a dataframe
+for file in csv_files:
+    df = pd.read_csv(file)
+    df_list.append(df)
+
+# Concatenate the dataframes into a single dataframe
+combined_df = pd.concat(df_list)
+
+# Write the combined dataframe to a new CSV file
+combined_df.to_csv("./docs/all_years.csv", index=False)
